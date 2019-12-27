70965beb3ff32d813b015c712dfa051da7148947
Mathieu PINSON <mpinson@e2r8p8.42.fr>
2019-12-27 16:43:01 +0100

    ok

---
+++ mon-projet-angular/src/app/appareil/appareil.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getColor() {
+    if (this.appareilStatus == "allumé") {
+      return "green"
+    }
+    return "red"
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+}
